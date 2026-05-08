cf44df54130d3335dd8c5ee47be1a956bd2f55c8
Kangila404 <ia3264666@gmail.com>
2026-05-08 21:37:49 +0900

    infra(main) : 초기 설정

---
+++ drift_log/src/main/java/org/example/drift_log/DriftLogApplication.java
@@ -0,0 +1,13 @@
+package org.example.drift_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriftLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriftLogApplication.class, args);
+    }
+
+}
---
+++ drift_log/src/test/java/org/example/drift_log/DriftLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.drift_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriftLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
