1c5ec0fb4cde883bc8c5b8855aa9774252508077
Kangila404 <ia3264666@gmail.com>
2026-05-11 20:28:06 +0900

    infra(server) : ci, spot_bog.xml, mySQL 설정, docker-compose, application.yml 파일 추가

--- drift_log/src/test/java/org/example/drift_log/DriftLogApplicationTests.java
+++ drift_log/src/test/java/org/example/drift_log/DriftLogApplicationTests.java
@@ -1,13 +1,23 @@
 package org.example.drift_log;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DriftLogApplicationTests {
+
+    @Value("${DB_PORT:없음}")
+    String dbPort;
+
+    @Value("${DB_NAME:없음}")
+    String dbName;
 
     @Test
     void contextLoads() {
+
     }
 
 }
