7e7fa919a3509d1a4958a777be22d810c8a5edf6
Kangila404 <ia3264666@gmail.com>
2026-05-19 14:29:01 +0900

    fix(date) : 하버네이트 적용 -> insert 되게 변경, test.yml에 flyway 제거

--- drift_log/src/main/java/org/example/drift_log/common/config/FlywayConfig.java
+++ drift_log/src/main/java/org/example/drift_log/common/config/FlywayConfig.java
@@ -4,11 +4,13 @@ import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class FlywayConfig {
 
     @Bean(initMethod = "migrate")
+    @DependsOn("entityManagerFactory")
     public Flyway flyway(DataSource dataSource) {
         return Flyway.configure()
             .dataSource(dataSource)
