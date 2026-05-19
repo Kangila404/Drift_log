0a15d55d84aa9def14f999d89462173e9c0b64f2
Kangila404 <ia3264666@gmail.com>
2026-05-19 14:40:27 +0900

    fix(data) : flywayConfig가 test 되지 않게 변경

--- drift_log/src/main/java/org/example/drift_log/common/config/FlywayConfig.java
+++ drift_log/src/main/java/org/example/drift_log/common/config/FlywayConfig.java
@@ -5,8 +5,10 @@ import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 public class FlywayConfig {
 
     @Bean(initMethod = "migrate")
